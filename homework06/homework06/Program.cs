9b438c36cb024c31e5471ca6de2a2dd6d0654c31
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2024-10-18 22:02:24 +0300

    Added needed variables

--- homework06/homework06/Program.cs
+++ homework06/homework06/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-
+            VendingMachine machineV1 = new VendingMachine("FirstMachine", 1000, 5.0, 4.0, 3.0, 2.0, 1.0);
+            Console.WriteLine(machineV1.Name);
         }
     }
 }
--- homework06/homework06/VendingMachine.cs
+++ homework06/homework06/VendingMachine.cs
@@ -1,7 +1,34 @@
 ﻿namespace homework06
 {
-    internal class VendingMachine
+    public class VendingMachine
     {
+        public string? Name { get; set; }
+        public double Balance { get; set; }
+        public double WaterLeft { get; set; }
+        public double CoffeeLeft { get; set; }
+        public double MilkLeft { get; set; }
+        public double SugarLeft { get; set; }
+        public double TotalSells { get; set; }
+        public VendingMachine(string name, double balance, double waterleft, double coffeeleft, double milkleft, double sugarleft, double totalsells)
+        {
+            Name = name;
+            if (balance >= 0) Balance = balance;
+            else Balance = 0;
+            WaterLeft = waterleft;
+            CoffeeLeft = coffeeleft;
+            MilkLeft = milkleft;
+            SugarLeft = sugarleft;
+            TotalSells = totalsells;
+        }
 
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{Name}-{Balance}-{WaterLeft}");
+        }
+
+        public void AddWater(int mass)
+        {
+            WaterLeft += 1.0;
+        }
     }
 }
